a80ecd4c823384a1a7792d2760c238b8857429c3
Ahmet Karabulut <a.karabulut070@gmail.com>
2022-06-27 19:53:41 +0200

    34.day

---
+++ day34_deneme/DenemeChild.java
@@ -0,0 +1,12 @@
+package day34_deneme;
+
+import static day34_accessMofifier_encapsulation.C01.halkaAcikSayi;
+
+public class DenemeChild {
+    public static void main(String[] args) {
+
+        System.out.println(halkaAcikSayi);
+        halkaAcikSayi=30;
+
+    }
+}
